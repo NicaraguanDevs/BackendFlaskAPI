eb0eb871228c9985bf96c5aed4e80a0947cd0f24
Dave Flr <daveflr@protonmail.com>
2020-02-26 02:27:49 -0600

    Serving flash to heroku

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+import os
 from src import app
 from models import db
 
 if __name__ == '__main__':
     db.bind(**app.config['PONY'])
     db.generate_mapping(create_tables=True)
-    app.run(debug=False)
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
