503021d99939f5c7563467ca8f71ac655885bd3a
Dave Flr <daveflr@protonmail.com>
2020-03-15 03:45:35 -0600

    Deploying

--- app.py
+++ app.py
@@ -6,5 +6,4 @@ if __name__ == '__main__':
     db.bind(**app.config['PONY'])
     db.generate_mapping(create_tables=True)
     # app.run(debug=True)
-    # app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
-    app.run()
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
